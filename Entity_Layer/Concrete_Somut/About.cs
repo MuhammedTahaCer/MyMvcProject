8079be47b31f3f433af825be96988beb634c7fd7
Muhammed Taha <cermuhammed720@gmail.com>
2022-09-09 14:34:59 +0300

    About ve Contactın oluşturulması

---
+++ Entity_Layer/Concrete_Somut/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer.Concrete_Somut
+{
+    internal class About
+    {
+        public int ID { get; set; }
+        public string AboutDetail1 { get; set; }
+        public string AboutDetail2 { get; set; }
+        public string AboutImage { get; set; }//Resimleri dosya yoluyla ayrı biryerden çekielbilir.
+        public string AboutImage2 { get; set; }
+    }
+}
---
+++ Entity_Layer/Concrete_Somut/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer.Concrete_Somut
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryNAme { get; set; }
+        public string State { get; set; }
+        public bool Statue { get; set; }//İlişkili tablolarda silme işlemi yerine durumunu aktif pasif etme
+    }
+}
