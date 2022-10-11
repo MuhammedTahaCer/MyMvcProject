d887214bb5ef1e959c6543e731e1e0c31432baec
Muhammed Taha <cermuhammed720@gmail.com>
2022-10-11 15:44:13 +0300

    EntityLayer içerisindeki structorların Data access ile haberleşmesi.

---
+++ DataAccess/EntityFrameWorks/EfCategoryDL.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.Repositories;
+using EntityLayer.Concrete_Somut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityFrameWorks
+{
+    public class EfCategoryDL: GenericRepository<Category>, ICategoryDL //bu şekilde hem category.cs miras alınıyor hemde ICategoryDL verileri kullanılabilir
+    {
+        //Böylece katmanlardaki ilişkili yapıları haberleştiriyorum
+        
+    }
+}
